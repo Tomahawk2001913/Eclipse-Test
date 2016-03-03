3d2bf24b2656fa2f0849732d390a246dd4416ea8
Tomahawk2001913 <tomahawk2001913@gmail.com>
2016-03-03 06:42:21 -0500

    Added ability to add numbers through the console.

--- Test Java Project/src/com/tomahawk2001913/test/Main.java
+++ Test Java Project/src/com/tomahawk2001913/test/Main.java
@@ -1,7 +1,17 @@
 package com.tomahawk2001913.test;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
+		
+		int ans = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		ans = scan.nextInt() + scan.nextInt();
+		
+		System.out.println(ans);
 	}
 }
